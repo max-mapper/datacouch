1b2fbb963f1bcd8a11fd760ef7839a7fc7e86463
Max Ogden <mogden@gmail.com>
2011-09-26 10:17:27 -0700

    test for proper creation and cleanup of databases

--- test/test_permissions.js
+++ test/test_permissions.js
@@ -5,24 +5,28 @@ var couch         = process.env['DATACOUCH_ROOT']
 var it = require('it-is')
   , _ = require('underscore')
   , request = require('request')
+  , async = require('async')
   , util = require('../processors/auth/couch_utils')
   ;
 
 function waitUntilExists(url, callback) {
-  request.head(url
-    , function(e,r,b) {
+  var start = new Date();
+  (function headRequest() {
+    request.head(url
+      , function(e,r,b) {
+        if( (new Date() - start) > 5000 ) callback(false)
         if(r.statusCode === 404) {
           setTimeout(function() {
-            console.log("waiting for " + url + " to exist");
-            waitUntilExists(url, callback);
-          }, 50)
+            headRequest(url);
+          }, 100)
         } else {
-          callback()
+          callback(true)
         }
       })
+  }())
 }
 
-function verifyCreated(doc, url) {
+function verifyCreated(doc, url, callback) {
   request.get({uri: url, json: true}
     , function(e,r,b) {
       it(404).equal(r.statusCode);
@@ -30,10 +34,18 @@ function verifyCreated(doc, url) {
         , function(e,r,b) {
           doc._rev = b.rev;
           it(201).equal(r.statusCode);
-          waitUntilExists(url, function() {
-            request.del(couch + '/' + doc._id);
+          waitUntilExists(url, function(ok) {
             doc._deleted = true;
-            request.post({uri: couch + '/datacouch', body: doc, json: true});
+            async.parallel({
+                db: function(cb) { request.del({uri: couch + '/' + doc._id, json: true}, cb) }
+              , doc: function(cb) { request.post({uri: couch + '/datacouch', body: doc, json: true}, cb) }
+              }
+              , function(err, results) {
+                it(ok).equal(true);
+                it(results.db[1].ok).equal(true);
+                it(results.doc[1].ok).equal(true);
+             }
+            )
           })
         })
     })
