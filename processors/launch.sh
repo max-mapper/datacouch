373060b65026546e970b7eb666bb097a1a08a9c8
Max Ogden <mogden@gmail.com>
2011-10-15 20:54:02 -0700

    add sharejs server processor

--- processors/launch.sh
+++ processors/launch.sh
@@ -4,4 +4,6 @@ cd ../auth && forever start auth_server.js
 cd ../backup && forever start backup_documents.js
 cd ../provision && forever start provision_databases.js
 cd ../stats && forever start compute_stats.js
+cd ../sharejs && forever start sharejs_server.js
+cd ../reverse_proxy && forever start proxy_server.js
 cd ../
