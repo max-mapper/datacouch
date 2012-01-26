e0c2cb5e7b298a353c25390280df904e7d2f36fa
Max Ogden <mogden@gmail.com>
2012-01-25 21:05:07 -0800

    removed unused url require

--- run.js
+++ run.js
@@ -3,8 +3,6 @@ logging.stdout()
 process.logging = logging
 
 var service = require('./service')() // we can pass debug/production options to this later
-  , url = require('url')
-  ;
 
 var t = service.createServer()
 t.listen(function () {
